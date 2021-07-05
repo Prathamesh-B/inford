5fd9a186742457a4e005380afe5f9f8dc71a96d4
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2021-07-05 11:14:11 +0530

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", encoding="utf-8") as readme:
 
 setup(
     name="inford",
-    version="1.0.0",
+    version="1.1.0",
     author="Prathamesh Bhaleakr",
     author_email="prathameshb1704@gmail.com",
     description="Simple module for sending dsicord webhooks",
